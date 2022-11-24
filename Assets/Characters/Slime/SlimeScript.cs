3599bd6a3fa897eb7b10f6c0e0e745687b99b408
Sven de Goede <Sven.d.goede@gmail.com>
2022-11-24 14:25:28 +0100

    Fixed slime animations
    Now has death animation and stops moving when dead

--- Assets/Characters/Slime/SlimeScript.cs
+++ Assets/Characters/Slime/SlimeScript.cs
@@ -8,7 +8,8 @@ public class SlimeScript : MonoBehaviour
     public float moveSpeed = 0.5f;
     public float hp = 1;
     private Color startColor;
-    public float getHitCooldown = 1f; 
+    public float getHitCooldown = 1f;
+    Animator animator;
     SpriteRenderer sprite;
 
 
@@ -17,6 +18,8 @@ public class SlimeScript : MonoBehaviour
         playerManager = GameObject.Find("PlayerManager");
         sprite = GetComponent<SpriteRenderer>();
         startColor = sprite.color;
+        animator = GetComponent<Animator>();
+        animator.SetBool("isMoving", true);
 
     }
 
@@ -26,6 +29,14 @@ public class SlimeScript : MonoBehaviour
         
         transform.position = Vector2.MoveTowards(transform.position, PlayerManager.instance.playerPos - new Vector2(0, 0.1f), moveSpeed * Time.deltaTime);
         getHitCooldown -= 0.1f * Time.fixedDeltaTime;
+
+        if (hp <= 0)
+        {
+            animator.SetBool("isMoving", false);
+            animator.SetBool("isDead", true);
+            moveSpeed = 0f;
+        }
+     
     }
 
     //check for collision
@@ -44,6 +55,7 @@ public class SlimeScript : MonoBehaviour
         }
     }
     
+
     void ChangeColorBack(){
         sprite.color = startColor;
     }
